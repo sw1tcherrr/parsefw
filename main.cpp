476e64f842f394ed1e21a3011c5fe8625befe2db
sw1tcherrr <sw1tcherrr@mail.ru>
2023-12-08 01:32:28 +0300

    error handling in main

--- main.cpp
+++ main.cpp
@@ -11,19 +11,30 @@ int main() {
     using namespace language::kotlin_func;
     using namespace pfw;
 
-    auto test = MakeMmap("test.txt").value();
+    auto buf_res = MakeMmap("test.txt");
+    if (!buf_res) { 
+        std::cerr << buf_res.error();
+        return -1;
+    }
+    auto buf = std::move(buf_res.value());
 
-    Lexer lex(test.begin(), test.begin());
+    Lexer lex(buf.begin(), buf.end());
     while (true) {
         Token tok = lex.NextToken();
-        std::cout << tok;
+        std::cout << tok << "\n";
         if (tok.index() == 0) {
             break;
         }
     }
 
-    Parser p(test.begin(), test.end());
-    auto ast = p.Parse().value();
+    Parser p(buf.begin(), buf.end());
+    auto ast_res = p.Parse();
+    if (!ast_res) {
+        std::cerr << ast_res.error();
+        return -1;
+    }
+    auto ast = std::move(ast_res.value());
+
     auto out = std::ofstream("ast.dot");
     pfw::graphviz::AstToDot(out, ast);
 }
