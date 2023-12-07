51d2ddb995919d13c048af3094140432e74583d3
sw1tcherrr <sw1tcherrr@mail.ru>
2023-12-08 01:35:20 +0300

    add line counting, ban non-byte inputs

--- lib/lexer_base.hpp
+++ lib/lexer_base.hpp
@@ -7,9 +7,11 @@
 
 namespace pfw {
 
-template <std::bidirectional_iterator I>
+template <std::contiguous_iterator I> 
+    requires std::same_as<std::remove_cv_t<std::iter_value_t<I>>, char>
 struct LexerBase {
     LexerBase(I begin, I end) : iter(std::move(begin)), end(std::move(end)) {
+        lines.push_back(iter);
     }
 
     LexerBase(LexerBase const&) = delete;
@@ -18,12 +20,16 @@ struct LexerBase {
 protected:
     I iter;
     I end;
+    std::vector<I> lines;
 
-    using ValueType = typename std::iterator_traits<I>::value_type;
+    using ValueType = typename std::iter_value_t<I>;
 
     template <std::predicate<ValueType> P>
     void Skip(P&& p) {
         while (Expect(std::forward<P>(p))) {
+            if (Expect('\n')) {
+                lines.push_back(iter);
+            }
             Consume();
         }
     }
