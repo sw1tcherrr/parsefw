3536a384c9ac47f97c62926b48600887cba1caf5
sw1tcherrr <sw1tcherrr@mail.ru>
2023-12-08 01:40:17 +0300

    refactoring of token_base - add source location - support std::format - TokenType template instead of macro - unify EOF with other tokens

--- lib/token_base.hpp
+++ lib/token_base.hpp
@@ -1,13 +1,22 @@
 #pragma once
 
 #include <concepts>
+#include <format>
 #include <string>
-
-namespace pfw {
-struct Eof {};
-}  // namespace pfw
+#include <variant>
+#include <ctll/fixed_string.hpp>
 
 namespace pfw::token {
+
+struct TokenBase {
+    size_t line;
+    size_t position;
+    std::string_view string_value;
+};
+
+std::string_view GetStringValue(TokenBase const& t) {
+    return t.string_value;
+}
 
 template <typename T>
     requires std::integral<T> || std::floating_point<T>
@@ -20,29 +29,43 @@ T GetNumericValue(Token const& t) {
     return t.numeric_value;
 }
 
-struct String {
-    std::string_view string_value;
-};
-
-template <std::derived_from<String> Token>
-std::string_view GetStringValue(Token const& t) {
-    return t.string_value;
-}
-
-struct Exact : String {};
-
-struct Variable : String {};
-
-std::string_view GetStringValue(Eof const& t) {
-    return "";
-}
+struct Exact : TokenBase {};
+struct Variable : TokenBase {};
 
 #define PFW_TOKEN(NAME, KIND, PATTERN)  \
 struct NAME : pfw::token::KIND { \
     static constexpr ctll::fixed_string kPattern = PATTERN; \
     static constexpr std::string_view kPatternStr = PATTERN; \
+    static constexpr std::string_view kName = #NAME;\
 };
 
-#define PFW_MAKE_TOKEN_TYPE(...) std::variant<pfw::Eof, __VA_ARGS__>
+PFW_TOKEN(END, TokenBase, "")
+
+template <typename... Tokens>
+using TokenType = std::variant<END, Tokens...>;
+
+template <typename... Tokens>
+std::ostream& operator<<(std::ostream& os, TokenType<Tokens...> const& t) {
+    os << std::format("{}", t);
+    return os;
+}
 
 }  // namespace pfw::token
+
+namespace std {
+template <typename... Tokens>
+struct std::formatter<pfw::token::TokenType<Tokens...>> : std::formatter<std::string> {
+    auto format(pfw::token::TokenType<Tokens...> const& token, std::format_context& ctx) const {
+        std::string res = std::visit(
+            [&]<std::derived_from<pfw::token::TokenBase> T>(T const& t) {
+                return std::format("{} : `{}` on line {} at postion {}",
+                    T::kName,
+                    t.string_value,
+                    t.line,
+                    t.position);
+        },
+        token);
+        return std::formatter<std::string>::format(res, ctx);
+    }
+};
+} // namespace std
