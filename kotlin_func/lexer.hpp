911bf3dd4625c1cc440fad0832b2a8307bfeec0e
sw1tcherrr <sw1tcherrr@mail.ru>
2023-12-08 01:45:34 +0300

    correctly parse EOF - it is not default case, it is parsed only when iter == end

--- kotlin_func/lexer.hpp
+++ kotlin_func/lexer.hpp
@@ -2,8 +2,11 @@
 
 #include <concepts>
 #include <ctre.hpp>
+#include <stdexcept>
+#include <string_view>
 
 #include "../lib/lexer_base.hpp"
+#include "../lib/util.hpp"
 #include "token.hpp"
 
 namespace language::kotlin_func {
@@ -32,44 +35,69 @@ struct Lexer : pfw::LexerBase<I> {
         auto start_pos = iter;
 
         auto exact_res = Parse<FUN>()
-                         | PFW_LAZY(Parse<LPAREN>)
-                         | PFW_LAZY(Parse<RPAREN>)
-                         | PFW_LAZY(Parse<LANGLE>)
-                         | PFW_LAZY(Parse<RANGLE>)
-                         | PFW_LAZY(Parse<COLON>)
-                         | PFW_LAZY(Parse<COMMA>)
-                         |= [] { return Token{Eof{}}; };
+                               | PFW_LAZY(Parse<LPAREN>)
+                               | PFW_LAZY(Parse<RPAREN>)
+                               | PFW_LAZY(Parse<LANGLE>)
+                               | PFW_LAZY(Parse<RANGLE>)
+                               | PFW_LAZY(Parse<COLON>)
+                               | PFW_LAZY(Parse<COMMA>)
+                               | PFW_LAZY(Parse<pfw::token::END>);
+                
 
         iter = start_pos;
 
-        auto variable_res = Parse<ID>() 
-                            |= [] { return Token{Eof{}}; };
+        auto variable_res = Parse<ID>()
+                                  | PFW_LAZY(Parse<pfw::token::END>);
 
         iter = start_pos;
 
-        auto ex_str = std::visit([](auto const& t) { return pfw::token::GetStringValue(t); }, exact_res);
-        auto var_str = std::visit([](auto const& t) { return pfw::token::GetStringValue(t); }, variable_res);
+        if (!exact_res && !variable_res) {
+            throw std::runtime_error(std::format("Can't parse any token on line {} at position {}", this->lines.size() - 1, iter - this->lines.back())); 
+            // todo don't throw
+        }
+
+        std::string_view ex_str = "";
+        if (exact_res) {
+            ex_str = std::visit(pfw::token::GetStringValue, exact_res.value());
+        }
+
+        std::string_view var_str = "";
+        if (variable_res) {
+            var_str = std::visit(pfw::token::GetStringValue, variable_res.value());
+        }
+
         if (ex_str.size() >= var_str.size()) {
             Base::Consume(ex_str.size());
-            return exact_res;
+            return exact_res.value();
         } else {
             Base::Consume(var_str.size());
-            return variable_res;
+            return variable_res.value();
         }
     }
 
 private:
     using Base::iter, Base::end;
 
-    template <std::derived_from<pfw::token::String> TokenType>
+    template <std::derived_from<pfw::token::TokenBase> TokenType>
     std::optional<Token> Parse() {
         auto match = ctre::starts_with<TokenType::kPattern>(iter, end);
         if (match) {
-            TokenType res{pfw::token::String{.string_value = std::string_view(iter, iter + match.size())}};
+            TokenType res{pfw::token::TokenBase
+                            {.line = this->lines.size() - 1,
+                            .position = size_t(iter - this->lines.back()),
+                            .string_value = std::string_view(iter, iter + match.size())}};
             Base::Consume(match.size());
             return {Token{std::move(res)}};
         }
-        return {};
+        return std::nullopt;
+    }
+
+    template <>
+    std::optional<Token> Parse<pfw::token::END>() {
+        if (iter == end) {
+            return {pfw::token::END{}};
+        }
+        return std::nullopt;
     }
 };
 
