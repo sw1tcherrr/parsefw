5c661b7014dd1bd95fde7031f43a1ba2a3fda14f
sw1tcherrr <sw1tcherrr@mail.ru>
2023-12-09 20:26:07 +0300

    new executable for experiments

---
+++ kotlin_func/test.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <ostream>
+
+#include "AST.hpp"
+#include "../lib/util.hpp"
+
+using namespace language::kotlin_func;
+using namespace language::kotlin_func::ast;
+
+void ToString(std::ostream& os, Node const& root) {
+    root.Visit(
+        [&](NtNode const& n) {
+            for (auto& c : n.Children()) {
+                ToString(os, c);
+            }
+        },
+        [&](TNode const& n) {
+            os << n.Label() << " ";
+        });
+}
+
+int main() {
+    Node root =
+    NtNode("Declaration", {
+        TNode(FUN("fun")),
+        TNode(ID("f")),
+        TNode(LPAREN("(")),
+        NtNode("Args", {
+            NtNode("Arg", {
+                TNode(ID("param")),
+                TNode(COLON(":")),
+                NtNode("Type", {
+                    TNode(ID("Int"))
+                })
+            })
+        }),
+        TNode(RPAREN(")"))
+    });
+
+    ToString(std::cout, root);
+    std::cout << "\n";
+}
